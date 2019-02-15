bc818a411320010922200ea8ff742a0a77e8e436
Simon Brown <hi@simon.am>
2019-02-15 14:17:11 +0000

    added instagram

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
     $('.heyyy').addClass('here-I-am');
   },2900); */
 
-  $(".img-bg--type").hover( function() {
+  /* $(".img-bg--type").hover( function() {
     var bgType = $(this).attr('data');
     // console.log('.full-background' + '.' + bgType);
     $('.img-bg' + '.' + bgType).toggleClass('view');
   });
-
+ */
 });
 
 
